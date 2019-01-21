8cd5b8c246827da4c5754d3caebf10b2d4ec696c
Abed Kadry <kadrya@campus.technion.ac.il>
2019-01-21 12:41:59 +0200

    yoyo

--- wet_files/Complex.h
+++ wet_files/Complex.h
@@ -5,8 +5,8 @@ namespace MtmMath {
     class Complex {
         double re,im;
     public:
-        Complex(double r=0, double i=0);
-        Complex operator=(const Complex& c) ;
+        Complex(double r=0.0, double i=0.0);
+        Complex& operator=(const Complex&) = default;
 
         Complex operator-() const;
 
@@ -17,15 +17,78 @@ namespace MtmMath {
         bool operator==(const Complex& c) const;
         bool operator!=(const Complex& c) const;
 
+        double Re() const;
+        double Im() const;
+
     };
-
-
-
 
     Complex operator+(const Complex& c1, const Complex& c2);
     Complex operator-(const Complex& c1, const Complex& c2);
     Complex operator*(const Complex& c1, const Complex& c2);
 
+
+    Complex::Complex(double r, double i) {
+        re = r;
+        im = i;
+    }
+
+/*Complex& Complex::operator=(const MtmMath::Complex &c) {
+    re = c.re;
+    im = c.im;
+    return *this;
+}
+*/
+    Complex Complex::operator-() const {
+        return Complex(-re, -im);
+    }
+
+    Complex& Complex::operator+=(const Complex &c) {
+        re += c.re;
+        im += c.im;
+        return *this;
+    }
+
+    Complex& Complex::operator-=(const Complex &c) {
+        return this->operator+=(-c);
+    }
+
+    Complex& Complex::operator*=(const Complex &c) {
+        int original = re;
+        re = re*(c.re) - im*(c.im);
+        im = original*(c.im) + im*(c.re);
+        return *this;
+    }
+
+    bool Complex::operator==(const Complex& c) const {
+        return (re == c.re && im == c.im);
+    }
+
+    bool Complex::operator!=(const Complex& c) const {
+        return !(this->operator==(c));
+    }
+
+    Complex operator+(const Complex& c1, const Complex& c2) {
+        Complex c = c1;
+        return c += c2;
+    }
+
+    Complex operator-(const Complex& c1, const Complex& c2) {
+        Complex c = c1;
+        return c -= c2;
+    }
+
+    Complex operator*(const Complex& c1, const Complex& c2) {
+        Complex c = c1;
+        return c.operator*=(c2);
+    }
+
+    double Complex::Re() const {
+        return re;
+    }
+
+    double Complex::Im() const {
+        return im;
+    }
 
 }
 
--- wet_files/test.cpp
+++ wet_files/test.cpp
@@ -1,16 +1,20 @@
 //
 // Created by Abed Kadry on 19-Jan-19.
 //
+#include <iostream>
 
 #include <iostream>
 
 #include "Complex.h"
 using namespace MtmMath;
+using namespace std;
 
 int main() {
     Complex wahad = Complex(2, 3);
     Complex thnen = Complex(4, 5);
+    thnen = -thnen;
     Complex thlath = wahad + thnen;
-    cout << "6+8i? " << thlath << endl;
-    cout << "-7-2i? " << wahd * thnen << endl;
+    cout << "6+8i? " << thlath.Re() << "+i" << thlath.Im() << endl;
+    cout << "-7+22i? " << (wahad * thnen).Re() << "+i" << (wahad * thnen).Im() << endl;
+    return 0;
 }
